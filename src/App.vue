934e53638008ec6a6b5985ee2353e69781cedbdb
sys <12345678@qq.com>
2020-01-18 14:08:47 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  justify-content: center;
+  justify-self: center;
+  color: #ffffff;
+}
+body{
+    background: #2c3e50;
+    width: 100%;
+    height: 100%;
+ }
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <Row type="flex" justify="center" align="middle" class-name="row a2">
+      <Col span="6">col-12</Col>
+      <Col span="6">col-12</Col>
+      <Col span="6">col-12</Col>
+      <Col span="6">col-12</Col>
+    </Row>
+    <Row type="flex" justify="center" align="middle" class-name="row a2">
+      <Col span="6">col-12</Col>
+      <Col span="6">col-12</Col>
+      <Col span="6">col-12</Col>
+      <Col span="6">col-12</Col>
+    </Row>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.a1{
+  height: 100px;
+  border-bottom: solid 1px  #ffffff;
+  text-align: center;
+}
+.a2{
+    background-color: black;
+    height: 50px;
+  }
+</style>
